14003f61b668c78fe4712fdc78fb91354d21a6b3
beatrizjardim031 <beatrizreisjardim@gmail.com>
2026-04-14 13:22:26 -0500

    Initial commit

---
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -0,0 +1,35 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class CellPhoneApplication {
+    public static void main(String[] args) {
+        CellPhone cellPhone = new CellPhone();
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("What is the serial number?: ");
+        cellPhone.setSerialNumber(input.nextLong());
+        input.nextLine();
+
+        System.out.print("What model is the phone?: ");
+        cellPhone.setModel(input.nextLine());
+
+        System.out.print("Who is the carrier?: ");
+        cellPhone.setCarrier(input.nextLine());
+
+        System.out.print("What is the phone number?: ");
+        cellPhone.setPhoneNumber(input.nextLine());
+
+        System.out.print("Who is the owner of the phone?: ");
+        cellPhone.setOwner(input.nextLine());
+
+        System.out.println("Serial number: " + cellPhone.getSerialNumber());
+        System.out.println("Model phone: " + cellPhone.getModel());
+        System.out.println("Carrier: " + cellPhone.getCarrier());
+        System.out.println("Phone number: " + cellPhone.getPhoneNumber());
+        System.out.println("Owner: " + cellPhone.getOwner());
+    }
+
+
+}
